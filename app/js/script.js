77608185d2aec9c1c87eb933ac089a9a6f93c293
qvoiriot <qvoiriot@gmail.com>
2016-06-11 16:43:28 +0200

    fixing scroll to mobile and add device detection

--- app/js/script.js
+++ app/js/script.js
@@ -3,12 +3,44 @@
 
   var site = {
 
+    is_mobile: false,
+    is_tablet: false,
+    is_desktop: false,
     mobile_nav_height: 50,
 
     init: function() {
       var self = this;
 
+      this.binds();
+      this.device_detection();
       this.bindClicks();
+    },
+
+    device_detection: function() {
+      this.is_mobile = false;
+      this.is_tablet = false;
+      this.is_desktop = false;
+
+      if (/force_mobile/.test(location.href) || $(window).width() <= 736 || ($('.resp-true').length > 0 && $(window).width() < 768)) {
+        this.is_mobile = true;
+      } else if (/force_tablet/.test(location.href) || $(window).width() <= 1024) {
+        this.is_tablet = true;
+      } else {
+        this.is_desktop = true;
+      }
+
+      this.$body.removeClass('mobile tablet desktop');
+      if (this.is_mobile) {
+        this.$body.addClass('mobile');
+      } else if (this.is_tablet) {
+        this.$body.addClass('tablet');
+      } else {
+        this.$body.addClass('desktop');
+      }
+    },
+
+    binds: function() {
+      this.$body = $('body');
     },
 
     bindClicks: function() {
@@ -42,6 +74,7 @@
     navigationActions: function() {
       $('.page-scroll').bind('click', function(event) {
         var $anchor = $(this).attr('data-anchor');
+        var $delta_mobile = 0;
 
         $('section').removeClass('scroll-down');
         $('.page-scroll').removeClass('active');
@@ -49,8 +82,12 @@
         $(this).addClass('active');
         $($anchor).addClass('scroll-down');
 
+        if (!this.is_desktop) {
+          $delta_mobile = site.mobile_nav_height;
+        }
+
         $('html, body').stop(true).animate({
-          scrollTop: $($anchor).offset().top - site.mobile_nav_height
+          scrollTop: $($anchor).offset().top - site.mobile_nav_height + $delta_mobile
         }, 1200, 'easeInOutExpo');
       });
     },
